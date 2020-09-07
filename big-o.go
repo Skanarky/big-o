12d90fc07439909513b8be7a86b26571b48617e0
Skanarky <kutkurov@gmail.com>
2020-09-07 15:29:05 -0600

    more methods

--- big-o.go
+++ big-o.go
@@ -18,3 +18,39 @@ func (m My) Worst() string {
 	fmt.Println(str)
 	return str
 }
+
+func (m My) Worse() string {
+	str := "A worse Big-O is O(2^n)"
+	fmt.Println(str)
+	return str
+}
+
+func (m My) Bad() string {
+	str := "A bad Big-O is O(n^2)"
+	fmt.Println(str)
+	return str
+}
+
+func (m My) OK() string {
+	str := "An okay Big-O is O(n log(n))"
+	fmt.Println(str)
+	return str
+}
+
+func (m My) Good() string {
+	str := "A good Big-O is O(n)"
+	fmt.Println(str)
+	return str
+}
+
+func (m My) Better() string {
+	str := "A better Big-O is O(log(n))"
+	fmt.Println(str)
+	return str
+}
+
+func (m My) Best() string {
+	str := "The best Big-O is O(1)"
+	fmt.Println(str)
+	return str
+}
